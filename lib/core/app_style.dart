c6c497e1e88ce6c70d95e9af788fff77e9427d1e
dewetbaumann <dewetbaumann@outlook.com>
2022-05-01 19:53:19 -0300

    se creo repo y se agrego el nav bar

---
+++ lib/core/app_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  final Color greyColor500 = const Color(0xFFBCBCBC);
+  final Color greyColor300 = const Color(0xFFEBEBEB);
+  final Color greyColor200 = const Color(0xFFEEEEEE);
+
+  final Color salmonColor = const Color(0xFFF97372);
+
+  final Color lightBlueColor500 = const Color(0xFF80b6f5);
+
+  final Color blueColor500 = const Color(0xFF2286DF);
+  final Color blueColor400 = const Color(0xFFE7F0FF);
+
+  final Color greenColor500 = const Color(0xFFA9E0A1);
+  final Color greenColor400 = const Color(0xFFEBF7E9);
+
+  final Color orangeColor500 = const Color(0xFFF6A693);
+  final Color orangeColor400 = const Color(0xFFFCEAE6);
+}
---
+++ lib/presentation/utils/routes.dart
@@ -0,0 +1,7 @@
+import 'package:facebook_clon/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+Map<String, Widget Function(BuildContext)> routes = {
+  '/home': (_) => const HomeScreen(),
+  // '/': (_) =>const ,
+};
