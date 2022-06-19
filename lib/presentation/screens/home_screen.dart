c9617bcd98ec1d0dbcd7c5d4792815f352af841e
dewetbaumann <dewetbaumann@outlook.com>
2022-06-19 16:46:31 -0300

    finalzo las tarjetas con los estados de los amigos en la barra para hacer post

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:facebook_clon/core/app/app_color.dart';
 import 'package:facebook_clon/presentation/widgets/action_bar/fc_action_bar.dart';
+import 'package:facebook_clon/presentation/widgets/friends_states/fc_friends_states.dart';
 import 'package:facebook_clon/presentation/widgets/make_post/fc_make_post.dart';
 import 'package:flutter/material.dart';
 
@@ -8,34 +9,14 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Size screen = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppColor.grey200,
       body: SafeArea(
         child: Column(
-          children: <Widget>[
-            const FcActionBar(),
-            const FcMakePost(),
-            Container(
-              width: screen.width,
-              height: 130,
-              color: Colors.grey,
-              padding: const EdgeInsets.all(5),
-              child: PageView(
-                children: <Widget>[
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.red,
-                  ),
-                  // Container()
-                  // FcCardFriends(),
-                  // FcCardFriends(),
-                  // FcCardFriends(),
-                  // FcCardFriends(),
-                ],
-              ),
-            ),
+          children: const <Widget>[
+            FcActionBar(),
+            FcMakePost(),
+            FcFriendsStates(),
           ],
         ),
       ),
